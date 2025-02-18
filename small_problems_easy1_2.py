9977b7a16cd75460f290059eb1d4c8628f8ce4f7
Jeff Campbell <jtcampbell399@gmail.com>
2025-02-18 06:18:06 -0600

    Add Easy 1, problem 3 and tweak problem 2

--- small_problems_easy1_2.py
+++ small_problems_easy1_2.py
@@ -1,7 +1,13 @@
-x = 1
+# x = 1
 
-while(x <= 99):
-    if(x % 2 != 0):
-        print(x)
-    x += 1
+# while(x <= 99):
+#     if(x % 2 != 0):
+#         print(x)
+#     x += 1
 
+starting_number = int(input('Enter the starting number :'))
+ending_number = int(input('Enter the ending number: '))
+
+for number in range(starting_number, ending_number + 1):
+    if(number % 2 != 0):
+        print(number)
